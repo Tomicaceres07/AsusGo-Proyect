030f35cc23306c8a6df39af1c59e217245828264
Tomi <tomicaceres07@gmail.com>
2022-05-31 12:04:14 -0300

    React updated to v18. Axios implemented. db.sql added. /api consumed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import React, { useEffect, useState } from 'react';
+const axios = require('axios').default;
 
 export const App = () => {
+
+  const [msj, setMsj] = useState();
+
+  useEffect(() => {
+    axios.get('/api')
+      .then(({data}) => {
+        setMsj(data.msj);
+      })
+      .catch((err) => {
+        setMsj(err);
+      })
+  }, [])
+  
   return (
     <div>
       <h1>Hi, this is React</h1>
+      <p>{msj}</p>
     </div>
   );
 }
