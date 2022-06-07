36c5b40ffc8ee07414a6cbe881048dab17824b08
Tomi <tomicaceres07@gmail.com>
2022-06-07 11:37:23 -0300

    Routes implemented. Bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState } from 'react';
-const axios = require('axios').default;
+import React from 'react';
+
+import { AppRouter } from './routers/AppRouter';
 
 export const App = () => {
-
-  const [msj, setMsj] = useState();
-
-  useEffect(() => {
-    axios.get('/api')
-      .then(({data}) => {
-        setMsj(data.msj);
-      })
-      .catch((err) => {
-        setMsj(err);
-      })
-  }, [])
-  
   return (
-    <div>
-      <h1>Hi, this is React</h1>
-      <p>{msj}</p>
-    </div>
+    <AppRouter />
   );
 }
---
+++ frontend/src/components/alumnos/AlumnosLoginScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AlumnosLoginScreen = () => {
+  return (
+    <div>AlumnosLoginScreen</div>
+  )
+}
