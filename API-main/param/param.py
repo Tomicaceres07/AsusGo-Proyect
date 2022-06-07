1224a314a54d7fe2b2277582d91873906be5c722
tomi medeot <tomimedeot@gmail.com>
2022-06-07 14:50:38 -0300

    Login Google Finished

--- API-main/param/param.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Request(BaseModel):
-    name: Optional[str] = None
-    password: Optional[str] = None
-    mail: Optional[str] = None
-    type: Optional[str] = None
-    code: Optional[str] = None
